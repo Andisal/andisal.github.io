77ae0b275745763229edca723b20d696419fbe69
Andisal <pavelmar102030@gmail.com>
2017-07-14 16:12:54 +0500

    edit

--- untitled1/js/app.js
+++ untitled1/js/app.js
@@ -1,15 +1,15 @@
-$(document).ready(function() {
-    $('a[href^="#"]').click(function(){
-        var el = $(this).attr('href');
-        $('body').animate({
-            scrollTop: $(el).offset().top}, 2000);
-        return false;
-    });
-});
-window.sr = ScrollReveal();
-sr.reveal('.navbar', {delay: 500});
-sr.reveal('.main', {delay: 700});
-sr.reveal('.main', {viewFactor: 0.01});
-sr.reveal('.main', {duration: 1500});
-sr.reveal('.row', {duration: 1000})
-sr.reveal('.row', {delay: 700})
+// $(document).ready(function() {
+//     $('a[href^="#"]').click(function(){
+//         var el = $(this).attr('href');
+//         $('body').animate({
+//             scrollTop: $(el).offset().top}, 2000);
+//         return false;
+//     });
+// });
+// window.sr = ScrollReveal();
+// sr.reveal('.navbar', {delay: 500});
+// sr.reveal('.main', {delay: 700});
+// sr.reveal('.main', {viewFactor: 0.01});
+// sr.reveal('.main', {duration: 1500});
+// sr.reveal('.row', {duration: 1000})
+// sr.reveal('.row', {delay: 700})
